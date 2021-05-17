7af2ed0189377c0b3c4cd6d0249e845aed296880
nekkron <26412490+nekkron@users.noreply.github.com>
2021-05-17 17:14:57 +0200

    Update Fix-BCMAgentConfigFile.ps1

--- Fix-BCMAgentConfigFile.ps1
+++ Fix-BCMAgentConfigFile.ps1
@@ -6,17 +6,17 @@ ATTRIB -r $path
 
 #Certificate Authority fix
 $text = (Get-Content -Path $path -ReadCount 0) -join "`n"
-$text -replace 'CertAuth=', 'CertAuth=bcm' | Set-Content -Path $path
+$text -replace 'CertAuth=', 'CertAuth=ABC' | Set-Content -Path $path
 #If config file already has correct value, this fixes the previous command to add it
 $text = (Get-Content -Path $path -ReadCount 0) -join "`n"
-$text -replace 'CertTrusted=', 'CertTrusted=bcm' | Set-Content -Path $path
+$text -replace 'CertTrusted=', 'CertTrusted=ABC' | Set-Content -Path $path
 
 #Certificate Trusted fix
 $text = (Get-Content -Path $path -ReadCount 0) -join "`n"
-$text -replace 'CertAuth=bcmbcm', 'CertAuth=bcm' | Set-Content -Path $path
+$text -replace 'CertAuth=bcmbcm', 'CertAuth=ABC' | Set-Content -Path $path
 #If config file already has correct value, this fixes the previous command to add it
 $text = (Get-Content -Path $path -ReadCount 0) -join "`n"
-$text -replace 'CertTrusted=bcmbcm', 'CertTrusted=bcm' | Set-Content -Path $path
+$text -replace 'CertTrusted=bcmbcm', 'CertTrusted=ABC' | Set-Content -Path $path
 
 # Mark file as read-only
 ATTRIB +R $path
