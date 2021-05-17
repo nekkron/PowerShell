836225170e89a79fd94d936b0c83b7f3e19ee533
nekkron <26412490+nekkron@users.noreply.github.com>
2021-05-17 17:35:50 +0200

    Update Uninstall-CylancePROTECT.ps1

--- Uninstall-CylancePROTECT.ps1
+++ Uninstall-CylancePROTECT.ps1
@@ -1,7 +1,7 @@
 ﻿# Uninstall Cylance PROTECT
 # https://support.cylance.com/s/article/How-to-Uninstall-CylancePROTECT62
 
-msiexec /x {2E64FC5C-9286-4A31-916B-0D8AE4B22954} /quiet UNINSTALLKEY="USO AV with 0 signature"
+msiexec /x {2E64FC5C-9286-4A31-916B-0D8AE4B22954} /quiet UNINSTALLKEY="CylancePassword"
 
 If ((Test-Path 'C:\Program Files\Cylance\Desktop\CyProtect.exe')) {
     Write-Error -Message "Cylance PROTECT is still installed!" -ErrorId 1618
