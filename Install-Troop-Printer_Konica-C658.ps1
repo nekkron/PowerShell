2d431f2c72d727e1c14fb5905fbca78242098cb1
nekkron <26412490+nekkron@users.noreply.github.com>
2021-05-17 17:23:38 +0200

    Update Install-Troop-Printer_Konica-C658.ps1

--- Install-Troop-Printer_Konica-C658.ps1
+++ Install-Troop-Printer_Konica-C658.ps1
@@ -1,7 +1,7 @@
 ﻿# Variables
 $driverDownloadPath = "https://dl.konicaminolta.eu/en/?tx_kmanacondaimport_downloadproxy[fileId]=73a7023c221c5196187099a44491c2df&tx_kmanacondaimport_downloadproxy[documentId]=5674&tx_kmanacondaimport_downloadproxy[system]=KonicaMinolta&tx_kmanacondaimport_downloadproxy[language]=EN&type=1558521685"
-$portName = "IP_192.168.10.10"
-$portAddress = "192.168.10.10" # Printer IP
+$portName = "IP_169.254.100.100"
+$portAddress = "169.254.100.100" # Printer IP
 $printerName = "Guest Printer" # This is the name the user will see when searching for printers
 $driverName = "KONICA MINOLTA C658SeriesPCL" # Must be exact. Install driver on your computer and copy its name and paste here
 # Do not modify these variables
