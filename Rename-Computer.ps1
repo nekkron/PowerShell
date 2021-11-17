20ddf09a77a3ffc4d96ee996ed52944873b9e92b
nekkron <26412490+nekkron@users.noreply.github.com>
2021-11-17 14:59:36 +0100

    Update Rename-Computer.ps1

--- Rename-Computer.ps1
+++ Rename-Computer.ps1
@@ -3,9 +3,6 @@
 ###   The purpose of this script is to automate the renaming of       ###
 ###   computers and joining workgroups during the imaging process     ###
 ###                                                                   ###
-###   This script was created by James Kasparek (jkasparek@uso.org)   ###
-###   Last modified on: 20180329                                      ###
-###                                                                   ###
 #########################################################################
 
 # Force Elevation of script
@@ -23,17 +20,14 @@ $NewCompName = Read-Host -Prompt 'Enter a new computer name'
 $NewCompName =$NewCompName.ToUpper()
 Rename-Computer -NewName "$NewCompName"
 
+if ($env:computerName.contains("USA-")) {
+    Add-Computer -WorkgroupName WKGP-USA
+}
 if ($env:computerName.contains("EUR-")) {
     Add-Computer -WorkgroupName WKGP-EUROPE
 }
 if ($env:computerName.contains("PAC-")) {
     Add-Computer -WorkgroupName WKGP-PACIFIC
-}
-if ($env:computerName.contains("SWA-")) {
-    Add-Computer -WorkgroupName WKGP-SWA
-}
-if ($env:computerName.contains("US-")) {
-    Add-Computer -WorkgroupName WKGP-USA
 }
 if ($env:computerName.contains("HQ-")) {
     Add-Computer -WorkgroupName WKGP-HQ
