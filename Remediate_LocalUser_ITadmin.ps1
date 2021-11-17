86e8b125d14ee1971f45b03cb9990b0712434280
nekkron <26412490+nekkron@users.noreply.github.com>
2021-11-17 14:56:51 +0100

    Update and rename Remediate_LocalUser_USOIT.ps1 to Remediate_LocalUser_ITadmin.ps1

---
+++ Remediate_LocalUser_ITadmin.ps1
@@ -0,0 +1,17 @@
+﻿#=============================================================================================================================
+#
+# Script Name:     Remediate-LocalUser_ITadmin.ps1
+# Description:     Remediates USOIT account issue if detected in Detect-LocalUser_ITadmin.ps1
+# Notes:           Changing the variable $securePassword changes the password of the USOIT account
+#
+#=============================================================================================================================
+
+# Variables
+$securePassword=ConvertTo-SecureString password -AsPlainText -Force
+
+# Reset password to admin account
+New-LocalUser -Name "IT Admin" -Password $securePassword -AccountNeverExpires -Description "IT Administrator Account" -PasswordNeverExpires  -ErrorAction SilentlyContinue
+Set-LocalUser -Name "IT Admin" -AccountNeverExpires -Description "IT Administrator Account" -Password $securePassword -PasswordNeverExpires $true
+
+# Add to administrators group
+Get-LocalGroupMember -Group Administrators -Member "IT Admin" -ErrorAction SilentlyContinue
