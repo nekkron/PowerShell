e6e21a8c3de95fd663ece5a528beb3b4423f648d
nekkron <26412490+nekkron@users.noreply.github.com>
2021-11-17 15:20:47 +0100

    Update Install-Printer_Ricoh-C306Z.ps1

--- Install-Printer_Ricoh-C306Z.ps1
+++ Install-Printer_Ricoh-C306Z.ps1
@@ -1,13 +1,14 @@
 ﻿# Variables
 $driverDownloadPath = "https://support.ricoh.com/bb/pub_e/dr_ut_e/0001311/0001311155/V1400/z91957L16.exe"
-$portName = "IP_169.254.100.100"
 $portAddress = "169.254.100.100" # Printer IP
-$printerName = "Office (C306Z PCL 6)" # This is the name the user will see when searching for printers
+$printerName = "RICOH C306Z PCL 6" # This is the name the user will see when searching for printers
 $driverName = "Gestetner MP C306Z PCL 6" # Must be exact. Install driver on your computer and copy its name and paste here
+
 # Do not modify these variables
 $driverDownloaded = "C:\support\PrinterDriver.zip"
 $extractPath = "C:\support\PrinterDriver"
 $driverPath = "C:\support\PrinterDriver\disk1"
+$portName = "IP_$portAddress"
 $portExists = Get-Printerport -Name $portname -ErrorAction SilentlyContinue
 $printerExists = Get-Printer -Name $printerName -ErrorAction SilentlyContinue
 
