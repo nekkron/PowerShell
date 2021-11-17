16e3978d59f2bf359e0abe9c17a192acdf55fea7
nekkron <26412490+nekkron@users.noreply.github.com>
2021-11-17 14:50:43 +0100

    Update Install-BCMAgent_macOS.ps1

--- Install-BCMAgent_macOS.ps1
+++ Install-BCMAgent_macOS.ps1
@@ -2,7 +2,7 @@
 # https://docs.bmc.com/docs/bcm129/installing-bmc-client-management-on-macos-by-using-the-pull-method-929640970.html
 
 
-Invoke-WebRequest "https://www.dropbox.com/s/b1q1hnxvo35663z/BCM_Agent?dl=1" -OutFile "/Applications/BCM_Agent"
+Invoke-WebRequest "https://www.dropbox.com/s/123456789/BCM_Agent?dl=1" -OutFile "/Applications/BCM_Agent"
 # Invoke-WebRequest "www.web.site" -OutFile "/Applications/BCM_Agent"
 chmod +x "/Applications/BCM_Agent"
 sudo "/Applications/BCM_Agent"
