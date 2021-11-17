d000d5879b912b3e45d9711aa613c83377093776
nekkron <26412490+nekkron@users.noreply.github.com>
2021-11-17 15:00:09 +0100

    Update Reset-BCMAgent_Task.ps1

--- Reset-BCMAgent_Task.ps1
+++ Reset-BCMAgent_Task.ps1
@@ -5,10 +5,7 @@
 $Trigger = New-ScheduledTaskTrigger -At 10:00am -Daily
 $User = "NT AUTHORITY\SYSTEM"
 $Action = New-ScheduledTaskAction -Execute powershell.exe -Argument "Restart-Service -Name 'BMC Client Management Agent'" -WorkingDirectory C:\Windows\SysWOW64\WindowsPowerShell\v1.0\
-#$Action = New-ScheduledTaskAction -Execute "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\PowerShell.exe" -Argument "Restart-Service -Name 'BMC Client Management Agent'"
-# -Argument "C:\PS\StartupScript.ps1"   #Can be used for locally stored PowerShell scripts
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -RestartCount 10 -RestartInterval (New-TimeSpan -Minutes 60) -RunOnlyIfNetworkAvailable -StartWhenAvailable -WakeToRun
 $Description = "This scheduled task automatically resets the BMC Client Management Agent daily to ensure proper communication with the remote server"
 
 Register-ScheduledTask -TaskName "BCM Agent - Reset (Daily)" -Trigger $Trigger -User $User -Action $Action -Settings $Settings -Description $Description -RunLevel Highest -Force
-
