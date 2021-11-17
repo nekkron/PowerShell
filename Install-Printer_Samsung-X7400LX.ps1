7a3ecd639cbc076028c25d977d2734893aef376c
nekkron <26412490+nekkron@users.noreply.github.com>
2021-11-17 15:21:19 +0100

    Update Install-Printer_Samsung-X7400LX.ps1

--- Install-Printer_Samsung-X7400LX.ps1
+++ Install-Printer_Samsung-X7400LX.ps1
@@ -1,12 +1,13 @@
 ﻿# Variables
 $driverDownloadPath = "https://www.dropbox.com/s/w5g8dczuj5jbahj/Printer.zip?dl=1"
-$portName = "IP_169.254.100.100" 
 $portAddress = "169.254.100.100" # Printer IP
 $printerName = "Samsung MultiXpress X7400LX" # This is the name the user will see when searching for printers
 $driverName = "Samsung X7600 Series" # Must be exact. Install driver on your computer and copy its name and paste here
+
 # Do not modify these variables
 $driverDownloaded = "C:\support\PrinterDriver.zip"
 $extractPath = "C:\support\PrinterDriver"
+$portName = "IP_$portAddress"
 $portExists = Get-Printerport -Name $portname -ErrorAction SilentlyContinue
 $printerExists = Get-Printer -Name $printerName -ErrorAction SilentlyContinue
 
