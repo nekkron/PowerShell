95b86857ae5700763c61a1b840ace547087ba5f5
nekkron <nekkron@gmail.com>
2022-07-22 17:05:44 +0200

    Delete Install-BCMAgent_macOS.ps1

--- Install-BCMAgent_macOS.ps1
+++
@@ -1,8 +0,0 @@
-﻿# This script is for deploying BMC Client Management Agent on macOS
-# https://docs.bmc.com/docs/bcm129/installing-bmc-client-management-on-macos-by-using-the-pull-method-929640970.html
-
-
-Invoke-WebRequest "https://www.dropbox.com/s/123456789/BCM_Agent?dl=1" -OutFile "/Applications/BCM_Agent"
-# Invoke-WebRequest "www.web.site" -OutFile "/Applications/BCM_Agent"
-chmod +x "/Applications/BCM_Agent"
-sudo "/Applications/BCM_Agent"
