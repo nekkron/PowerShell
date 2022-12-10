f164d595994b478484b923ecb58660cdea1bc42d
James Kasparek <nekkron@gmail.com>
2022-12-11 00:14:15 +0100

    Update Configure-DellCommandUpdate.ps1
    changed the log file to a specific folder & date/time file name so logs aren't being overwritten. 
    these logs should only be created once. scheduled updates should have a standard name to prevent hard drives from overfilling

--- Dell/Configure-DellCommandUpdate.ps1
+++ Dell/Configure-DellCommandUpdate.ps1
@@ -2,7 +2,7 @@
 	.NOTES
 	===========================================================================
 	 Created with: 	Windows PowerShell ISE x64
-	 Created on:   	2022-06-20
+	 Created on:   	2022-12-11
 	 Created by:   	James Kasparek
 	 Organization: 	United Service Organizations
 	 Filename:     	Configure-DellCommandUpdate.ps1
@@ -15,16 +15,19 @@
 
 # Variables
 $DCUcli = 'C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe'
+$timestamp = Get-Date -UFormat "%Y%m%d-%R" | ForEach-Object { $_ -replace ":", "." }
 
 if(!(Test-Path -Path $DCUcli)) {
-    Write-Warning "Dell Command Update is not installed on this machine!"
-    Write-Error -Message "DCU CLI is not installed on this device!"
-    Exit
+    Write-Error -Message "Dell Command Update is not installed on this device!"
+    Exit 1
     }
 Write-Host "Dell Command Update | Command Line Interface is installed on this device!" -ForegroundColor Green
 Write-Host "Configuring Dell Command Update"
-cmd /c $DCUcli /configure -silent -autoSuspendBitLocker=enable -userConsent=disable
+#Start-Process -FilePath $DCUcli -ArgumentList (/configure -silent -autoSuspendBitLocker=enable -userConsent=disable -outputlog=C:\ProgramData\Dell\logs\dcu-configure.log)
+cmd /c $DCUcli /configure -silent -autoSuspendBitLocker=enable -userConsent=disable -outputLog=C:\ProgramData\Dell\logs\configure\$timestamp.log
 Write-Host "Scanning for Dell updates"
-cmd /c $DCUcli /scan -outputLog=C:\ProgramData\Dell\logs\dcu-cli.log
+#Start-Process -FilePath $DCUcli -ArgumentList (/scan -outputLog=C:\ProgramData\Dell\logs\dcu-scan-$date.log)
+cmd /c $DCUcli /scan -outputLog=C:\ProgramData\Dell\logs\scan\$timestamp.log
 Write-Host "Applying Dell updates, reboot disabled."
-cmd /c $DCUcli /applyUpdates -reboot=disable -outputLog=C:\ProgramData\Dell\logs\dcu-cli.log
+#Start-Process -FilePath $DCUcli -ArgumentList (/applyUpdates -reboot=disable -outputLog=C:\ProgramData\Dell\logs\dcu-apply-$date.log)
+cmd /c $DCUcli /applyUpdates -reboot=disable -outputLog=C:\ProgramData\Dell\logs\updates\$timestamp.log
