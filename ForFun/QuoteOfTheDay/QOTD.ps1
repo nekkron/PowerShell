2c8810a6c812acba412f3fc801861c797819c379
James Kasparek <nekkron@gmail.com>
2023-10-05 00:18:32 +0200

    Create QOTD.ps1

--- ForFun/QuoteOfTheDay/QOTD.ps1
+++ ForFun/QuoteOfTheDay/QOTD.ps1
@@ -1,4 +1,4 @@
 Add-Type -AssemblyName System.Speech
-Invoke-WebRequest 'https://raw.githubusercontent.com/nekkron/PowerShell/main/ForFun/QuoteOfTheDay.txt' -OutFile $env:USERPROFILE/QOTD.txt
+Invoke-WebRequest 'https://raw.githubusercontent.com/nekkron/PowerShell/main/ForFun/QuoteOfTheDay/QOTD.txt' -OutFile $env:USERPROFILE/QOTD.txt
 $Speaker = [Speech.Synthesis.SpeechSynthesizer]::new()
 $Speaker.Speak((Get-Random -InputObject (Get-Content "$env:USERPROFILE/QOTD.txt")))
