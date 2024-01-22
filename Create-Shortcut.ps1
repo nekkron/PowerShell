61714cc223e2df7b02fcc0971e7968a775e4da5e
James Kasparek <nekkron@gmail.com>
2024-01-22 09:43:05 +0100

    Update Create-Shortcut.ps1

--- Create-Shortcut.ps1
+++ Create-Shortcut.ps1
@@ -1,13 +1,13 @@
-Start-Transcript $env:ProgramData\Create-Shortcut.log                                  # Create local log file
+Start-Transcript $env:ProgramData\Create-Shortcut.log           # Create local log file
 
 # Variables
-$IconLocationLocal = "$env:Windir\icon.ico"                                            # Local path to icon
-$IconLocationRemote = "url to .ico"                                                    # Remote path to icon
-$HelpShortcut = "$env:Public\Desktop\Shortcut.lnk"                                     # Shortcut location and name
-$ShortcutDescription = "Click here to open the application"                            # Hover over description text
-$ShortcutFile = "URL shortcut or application"                                          # Program to execute
-$ShortcutHotkey = "CTRL+SHIFT+H"                                                       # Keyboard shortcut
-$ShortcutWindowStyle = 1                                                               # Window size [1=Normal] [3=Maximized] [7=Minimized]
+$IconLocationLocal = "$env:Windir\icon.ico"                     # Local path to icon
+$IconLocationRemote = "url to .ico"                             # Remote path to icon
+$HelpShortcut = "$env:Public\Desktop\Shortcut.lnk"              # Shortcut location and name
+$ShortcutDescription = "Click here to open the application"     # Hover over description text
+$ShortcutFile = "URL shortcut or application"                   # Program to execute
+$ShortcutHotkey = "CTRL+SHIFT+H"                                # Keyboard shortcut
+$ShortcutWindowStyle = 1                                        # Window size [1=Normal] [3=Maximized] [7=Minimized]
 $UserDir = Get-ChildItem "C:\Users" -Directory
 
 # Creating the shortcut
