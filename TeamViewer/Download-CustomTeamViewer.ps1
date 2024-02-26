1617c7669a9d282656354c85a3d843d5a8b3dfec
James Kasparek <nekkron@gmail.com>
2024-02-26 17:29:22 +0100

    Removed redundancies

--- TeamViewer/Download-CustomTeamViewer.ps1
+++ TeamViewer/Download-CustomTeamViewer.ps1
@@ -1,39 +1,36 @@
+# https://community.teamviewer.com/English/discussion/120192/automation-download-custom-teamviewer-with-powershell
 # Download path
-$downloadpath = "C:\TeamViewer"
+$downloadpath = "$env:ProgramData\TeamViewer"
 # URL of your customized TeamViewer
 $URL = "https://get.teamviewer.com/<yourcustomURL>"
 
 # Function to create the browser object, navigate to the website, isolate the download url and download the TeamViewer QS
-function Download_TeamViewerQS () {
+function Download_TeamViewer () {
     # Internet Explorer application to fool the website into thinking the connection is a client session (this is needed to return the result from javascript function that generates the downloadlink of your custom TeamViewer using the newest version of it)
     $iebrowser = New-Object -ComObject InternetExplorer.Application
     # navigates to the given Website/URL
     $iebrowser.Navigate($URL)
-    # Pausing 3 seconds because creating the browser object and navigating to the website takes a little while depending on your system and internet connection
+    # Pause for 3 seconds to creating the browser object as navigating to the website takes a little while depending on your system and internet connection
     Start-Sleep -Seconds 3
     # isolates the generated download URL of the custom TeamViewer
-    $CustomTVQS_URL = $iebrowser.Document.getElementById('MasterBodyContent_btnRetry').href
+    $CustomTV_URL = $iebrowser.Document.getElementById('MasterBodyContent_btnRetry').href
     # Downloads the custom TeamViewer QS
-    Start-BitsTransfer -source $CustomTVQS_URL -Destination $downloadpath\TeamViewerQS.exe       
+    Start-BitsTransfer -source $CustomTV_URL -Destination $downloadpath\TeamViewer_Custom.exe       
 }
 
 # Try (creating path and) downloading and catch problem if this is not possible
 try {
-    Write-Host "Downloading custom TeamViewer QuickSupport..." -BackgroundColor Black -ForegroundColor Yellow
-    # Tests if path exists and (creates the path and) downloads TeamViewer QS into it
-    if (Test-Path $downloadpath) {
-        # Calls the download function
-        Download_TeamViewerQS
-    } else {
+    Write-Host "Downloading custom TeamViewer..." -BackgroundColor Black -ForegroundColor Yellow
         # Creates the download path if not existent
         New-Item -Path $downloadpath -ItemType Directory | Out-Null
         # Calls the download function
-        Download_TeamViewerQS
-    }
+        Download_TeamViewer
 } catch {
     # Display error if one occurs
     $Error
     # Write information for user
     Write-Host "Download can not be started.`n`nReason: The security settings for `"Internet Zone`" in your Internet Explorer need following configuration:`n- Filedownloads need to be activated`n- Active Scripting needs to be activated" -ForegroundColor Yellow -BackgroundColor Black
     Start-Sleep -Seconds 10
+    Exit 1
 }
+Exit 0
