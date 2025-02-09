ac8eb9d330310434ada00e2c7ce6de0d777682e3
James Kasparek <nekkron@gmail.com>
2025-02-09 23:46:20 +0100

    cleanup

--- Update-NTPServer.ps1
+++
@@ -1,3 +0,0 @@
-﻿# Change Windows time sync location and force time sync
-# https://docs.microsoft.com/en-us/windows-server/networking/windows-time-service/windows-time-service-tools-and-settings
-w32tm /config /manualpeerlist:"time.windows.com" /syncfromflags:manual /update
