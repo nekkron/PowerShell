ac07852f9c7dfcda42547f2234823efcb220c93e
James K <nekkron@gmail.com>
2025-06-13 08:46:30 -0700

    Delete Microsoft/Teams/Deploy-TeamsCacheClearer.ps1

--- Microsoft/Teams/Deploy-TeamsCacheClearer.ps1
+++
@@ -1,9 +0,0 @@
-﻿# This script will place the Teams Cache Remover inside of the support folder on the local computer
-
-# Create support folder
-New-Item -ItemType Directory -Force -Path C:\support
-
-# Where the real work happens. Ensure Output file extension is the same as website file extension!
-Invoke-WebRequest "https://bit.ly/3lhJJ8e" -OutFile "C:\support\MicrosoftTeams-ClearCache.ps1"
-
-Set-ExecutionPolicy RemoteSigned -Force
