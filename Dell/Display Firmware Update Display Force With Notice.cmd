1075cd34c37ce2a539a3fdb6689bd2105769af3d
James Kasparek <nekkron@gmail.com>
2026-07-13 18:21:37 +0200

    Add firmware update script for Dell displays
    Create a script to force display firmware update with notice.

---
+++ Dell/Display Firmware Update Display Force With Notice.cmd
@@ -0,0 +1,4 @@
+cd c:\ 
+md DDPMCLI 
+cd C:\Program Files\Dell\Dell Display and Peripheral Manager\Plugins\Subagent 
+CLI.Subagent /set -app=FirmwareUpdate -value=Display,forceWithNotice >>C:\DDPMCLI\FWUpdatedForceWithNotice.log
